1d7e15614e457f4d791c7f332c1034a704d7c194
Alvin Huang <alvin@hashicorp.com>
2019-03-27 11:41:53 -0400

    initial

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate-densify-demo"
+    key    = "demo.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  count         = 2
+
+  tags {
+    Name = "instance-${terraform.workspace}-${count.index}"
+  }
+}
